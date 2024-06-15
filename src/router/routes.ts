74d34a2de6546b6afeb469a068da5f14263d5e0b
Mr.Xu Fengchun <thomsenw005@gmail.com>
2024-06-15 17:57:10 +0800

    基于Vant4 官方搭建的脚手架  官方地址如下

---
+++ src/router/routes.ts
@@ -0,0 +1,42 @@
+import Layout from "@/layout/index.vue";
+import type { RouteRecordRaw } from "vue-router";
+import Home from "@/views/home/index.vue";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "root",
+    component: Layout,
+    redirect: { name: "Home" },
+    children: [
+      {
+        path: "home",
+        name: "Home",
+        component: Home,
+        meta: {
+          title: "主页"
+          
+        }
+      },
+      {
+        path: "tools",
+        name: "Tools",
+        component: () => import("@/views/tools/index.vue"),
+        meta: {
+          title: "视频"
+        }
+      },
+      // {
+      //   path: "about",
+      //   name: "About",
+      //   component: () => import("@/views/about/index.vue"),
+      //   meta: {
+      //     title: "关于",
+      //     noCache: true
+      //   }
+      // }
+    ]
+  }
+];
+
+export default routes;
